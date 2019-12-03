99f4536f23e5267e884a8420a182de3b642775ec
goodera <1972387389@qq.com>
2019-12-03 14:58:15 +0800

    Hello Git

--- gitJava/src/Git.java
+++ gitJava/src/Git.java
@@ -2,5 +2,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("My useful GitHub");
+        System.out.println("CLIDE");
     }
 }
